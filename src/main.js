329f96bc30206cfcbbcd078ee554de1fce02f893
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-20 08:57:57 -0800

    add test for .env and API_KEY security for push

--- src/main.js
+++ src/main.js
@@ -2,3 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from './services/exchangeRate.js';
+
+//Business Logic//
+
+function displayExchange(response) {
+
+}
+
+//User Interface Logic//
+
+$(document).ready(function() {
+
+});
