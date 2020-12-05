37e335128265d5af963b939284b9cc24c24462ce
HarrisonStrand <harrisonstrand@gmail.com>
2020-12-05 15:28:32 -0800

    add else statement to finish displayExchange function

--- src/main.js
+++ src/main.js
@@ -9,8 +9,14 @@ import Exchange from './services/exchangeRate.js';
 function displayExchange(response, usDollar, currency) {
   if ((!currency) && (response.status != 'success')) {
     $('#displayExchange').text(`There was an error: ${response.message}`);
+  } else if (!currency) {
+    $('#displayExchange').text('Please select a currency in first box');
+  } else if (usDollar === '') {
+    $('#displayExchange').text('Please select USD amount in second box');
   } else if (response.result === 'success') {
     $('#displayExchange').text(`${usDollar} in ${currency}: ${response.conversion_rates[currency]*usDollar}`); 
+  } else {
+    $('#displayExchange').text(`There was an error: ${response.message}`);
   }
 }
 
@@ -22,8 +28,10 @@ $(document).ready(function() {
     event.preventDefault();
 
     let usDollar = $('#usDollar').val();
+    console.log(usDollar);
     $('#usDollar').val("");
     let currency = $('input:radio:checked').val();
+    console.log(currency);
     Exchange.getExchange()
       .then(function(response) {
         displayExchange(response, usDollar, currency);
